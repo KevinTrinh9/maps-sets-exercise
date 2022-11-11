e8ff76099ce37955f95c3b4675cd6a97ae66d571
Kevin Trinh <kevintrinh@Computer.local>
2022-11-10 21:07:32 -0800

    add file

---
+++ maps-sets-exercise.js
@@ -0,0 +1,37 @@
+// Quick Question #1
+// What does the following code return?
+new Set([1,1,2,2,3,4]) // should return {1, 2, 3, 4}
+
+// Quick Question #2
+// What does the following code return?
+[...new Set("referee")].join("") // should return "ref"
+
+// Quick Question #3
+// What does the Map m look like after running the following code?
+let m = new Map()
+m.set([1,2,3], true)
+m.set([1,2,3], false)
+// 0: {Array(3) => true}
+// 1: {Array(3) => false}
+
+// hasDuplicate
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+// vowelCount
+function isVowel(char) {
+    return "aeiou".includes(char)
+}
+function vowelCount(str) {
+    const vowelMap = new Map()
+    for (let char of str) {
+        let lowerCaseChar = char.toLowerCase()
+        if (isVowel(lowerCaseChar)) {
+            if (vowelMap.has(lowerCaseChar)) {
+                vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1)
+            } else {
+                vowelMap.set(lowerCaseChar, 1)
+            }
+        }
+    }
+    return vowelMap
+}
